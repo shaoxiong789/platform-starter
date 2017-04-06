721da2bbd7a9a55626b5a7f16028082e7aedb80c
丁少雄 <380449900@qq.com>
2017-04-06 17:56:24 +0800

    Update 

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
+const spawn = require('child_process').spawn;
+
+setInterval(function(){
+  var git = spawn('git', ['pull']);
+  git.stdout.on('data', (data) => {
+    console.log(data.indexOf('Already up-to-date'))
+    console.log(`stdout: ${data}`);
+  });
+},30000)
+
+
 app.use(express.static('dist'));
-var server = app.listen(8081, function () {
+var server = app.listen(8081,'127.0.0.1' ,function () {
 
   var host = server.address().address
   var port = server.address().port
