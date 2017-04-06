e7eee3a2a81cc7ba533aca21b75194752a6391ce
丁少雄 <380449900@qq.com>
2017-04-06 12:55:46 +0800

    update 

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('dist'));
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
